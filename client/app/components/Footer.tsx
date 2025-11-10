dbacde49905399b82a7710a94a6a16d7a2fd0940
Gabriel D <gabriel.dalibert@gmail.com>
2025-11-10 11:35:30 +0100

    chore ( footer) : change in the autor

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center py-6 border-t mt-8 text-gray-500 bg-white">
-      © {new Date().getFullYear()} Gabriel DALIBERT · Built with Next.js + TypeScript
+      © {new Date().getFullYear()} Gabriel DALIBERT - Rayan GAAD - Kamil BENJELLOUN · Built with Next.js + TypeScript
     </footer>
   )
 }
