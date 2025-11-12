dfead44c9a903ac4c790cbb0def535781522dcd1
RayanGAAD <rayan.gaad@edu.ece.fr>
2025-11-12 11:25:57 +0100

    chore: ajout  next.config

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["annzicukozdmiszdfbci.supabase.co"],
+    domains: [
+      "annzicukozdmiszdfbci.supabase.co", // ton domaine Supabase
+      "via.placeholder.com",              // domaine pour les affiches par défaut
+    ],
   },
 };
 
